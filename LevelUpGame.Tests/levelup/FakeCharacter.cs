c8af9d65ff81a41b3bbecd4394eb1927cf7fcac9
Garrett Morgan <gmorgan0913@gmail.com>
2023-11-16 14:41:00 -0500

    Adding fake controller file

---
+++ LevelUpGame.Tests/levelup/FakeCharacter.cs
@@ -0,0 +1,24 @@
+using NUnit.Framework;
+using levelup;
+
+namespace levelup
+{
+    [TestFixture]
+    public class CharacterTest
+    {
+        private  CharacterClass? testObj;
+
+        [Setup]
+        public void SetUp()
+        {
+            testObj = new GameCharacter();
+        }
+
+        [Test]
+        {
+            
+        }
+
+
+    }
+}
