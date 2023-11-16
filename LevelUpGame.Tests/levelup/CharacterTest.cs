1fa1c252a91bfa8755acd88b79b8efaacedc7a3b
Luc Vantroys <lootchino@hotmail.com>
2023-11-16 14:31:16 -0500

    add first character unit test

---
+++ LevelUpGame.Tests/levelup/CharacterTest.cs
@@ -0,0 +1,24 @@
+using NUnit.Framework;
+using levelup;
+
+namespace levelup
+{
+    [TestFixture]
+    public class CharacterTest
+    {
+        private Character? testObj;
+
+        [SetUp]
+        public void SetUp()
+        {
+            testObj = new Character();
+        }
+
+        [Test]
+        public void IsCharacterNameInitialized()
+        {
+#pragma warning disable CS8602 // Rethrow to preserve stack details
+            Assert.IsNotNull(testObj.GetName());
+        }
+    }
+}
